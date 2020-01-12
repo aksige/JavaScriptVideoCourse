1255ca2a09b9332a9effb76e1630b9338e71200d
aksige <aksige@gmail.com>
2020-01-12 04:15:53 -0800

    add code

--- question/js/script.js
+++ question/js/script.js
@@ -146,8 +146,75 @@ function randomColors() {
     var choices = ['btn-primary', 'btn-danger', 'btn-success', 'btn-warning']
 
     for (let i = 0; i < all_buttons.length; i++) {
-        var randomNumber = Math.floor(Math.random()*4);
+        var randomNumber = Math.floor(Math.random() * 4);
         all_buttons[i].classList.remove(all_buttons[i].classList[1]);
         all_buttons[i].classList.add(choices[randomNumber]);
     }
+}
+
+// Challenge 5: BlackJack
+
+let blackjackGame = {
+    'you': { 'scoreSpan': '#your-blackjack-result', 'div': "#your-box", 'score': 0 },
+    'dealer': { 'scoreSpan': '#dealer-blackjack-result', 'div': "#dealer-box", 'score': 0 },
+    'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'J', 'Q', 'A'],
+    'cardsMap': { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'K': 10, 'J': 10, 'Q': 10, 'A': [1, 11] }
+};
+
+const YUO = blackjackGame['you']
+const DEALER = blackjackGame['dealer']
+
+const hitSound = new Audio('sounds/we-2.mp3')
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+
+function blackjackHit() {
+    let card = randomCard();
+    console.log(card);
+    showCard(card, YUO);
+    updateScore(card, YUO);
+    showScore(YUO);
+    console.log(YUO['score']);
+}
+
+function randomCard() {
+    let randomIndex = Math.floor(Math.random() * 13);
+    return blackjackGame['cards'][randomIndex];
+}
+
+function showCard(card, activePlayer) {
+    let cardImage = document.createElement('img');
+    cardImage.src = `img/${card}.png`;
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+    hitSound.play();
+}
+
+function blackjackDeal() {
+    let yourImages = document.querySelector('#your-box').querySelector('img');
+    let dealerImages = document.querySelector('#dealer-box').querySelector('img');
+
+    for (i = 0; i < yourImages.length; i++) {
+        yourImages[i].remove();
+    }
+
+    for (i = 0; i < dealerImages.length; i++) {
+        dealerImages[i].remove();
+    }
+}
+
+function updateScore(card, activePlayer) {
+    if (card === 'A') {
+        if (activePlayer['score'] + blackjackGame['cardsMap'][card][1] <= 21) {
+            activePlayer['score'] += blackjackGame['cardsMap'][card][1];
+        } else {
+            activePlayer['score'] += blackjackGame['cardsMap'][card][0];
+        }
+    } else {
+        activePlayer['score'] += blackjackGame['cardsMap'][card];
+    }
+}
+
+function showScore(activePlayer) {
+    document.querySelector(activePlayer['scoreSpan']).textContent = activePlayer['score'];
 }
